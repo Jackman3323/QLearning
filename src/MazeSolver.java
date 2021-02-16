68d603994d8d0911fd8b2b9870f27f1f8c5b6e9b
jgecko11 <jgreco22@kentdenver.org>
2021-02-16 12:32:51 -0700

    3x3

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -13,14 +13,14 @@ class QLearning {
     private final double alpha = 0.1; // Learning rate
     private final double gamma = 0.9; // Eagerness - 0 looks in the near future, 1 looks in the distant future
 
-    private final int mazeWidth = 10;
-    private final int mazeHeight = 5;
+    private final int mazeWidth = 3;
+    private final int mazeHeight = 3;
     private final int statesCount = mazeHeight * mazeWidth;
 
     private final int reward = 100;
     private final int penalty = -10;
 
-    private Maze mazeGenerator = new Maze(10, 5);
+    private Maze mazeGenerator = new Maze(3, 3);
 
     private String[][] maze;  // Maze read from file
     private int[][] R;       // Reward lookup
